12006ec4dd4b9f3b68864a17a07977d549f91d17
c0rax-3 <221375761+c0rax-3@users.noreply.github.com>
2026-02-15 15:28:52 +0000

    Create Marker Class and basic methods

---
+++ noughts-and-crosses.js
@@ -0,0 +1,46 @@
+class Marker {
+    #symbol = '';
+
+    constructor(symbol) {
+        this.#symbol = symbol;
+    }
+
+    isNought() {
+        return this.#symbol === 'O'
+    }
+
+    isCross() {
+        return this.#symbol === 'X'
+    }
+
+    isBlank() {
+        return this.#symbol === 'blank'
+    }
+
+    equals(otherMarker) {
+        return this.#symbol === otherMarker.#symbol;
+    }
+}
+
+class Nought extends Marker {
+    constructor() {
+        super('O');
+    }
+}
+
+class Cross extends Marker {
+    constructor() {
+        super('X');
+    }
+}
+
+class BlankMarker extends Marker {
+    constructor() {
+        super('blank');
+    }
+}
+
+const marker1 = new BlankMarker();
+
+const marker2 = new BlankMarker();
+console.log(marker1.equals(marker2))
