a9bd7eb393ee94d808fb251e924afd4b11744736
c0rax-3 <221375761+c0rax-3@users.noreply.github.com>
2026-03-15 11:02:03 +0000

    Add check end method to board

--- noughts-and-crosses.js
+++ noughts-and-crosses.js
@@ -79,12 +79,70 @@ class Board {
     }
 
     isOccupied(row, column) {
-        return this.cells[row][column].isBlank()
+        return !(this.cells[row][column].isBlank())
+    }
+
+    checkEndCondition() {
+        if (this.getCell(0,0) === this.getCell(0,1) && this.getCell(0,1) === this.getCell(0,2) 
+        && this.isOccupied(0,0)) {
+            return this.getCell(0,0)
+        }
+
+        if (this.getCell(1,0) === this.getCell(1,1) && this.getCell(1,2) === this.getCell(1,0) 
+        && this.isOccupied(1,0)) {
+            return this.getCell(1,0)
+        }
+
+        if (this.getCell(2,0) === this.getCell(2,1) && this.getCell(2,0) === this.getCell(2,2) 
+        && this.isOccupied(2,0)) {
+            return this.getCell(2,0)
+        }
+
+        if (this.getCell(0,0) === this.getCell(1,0) && this.getCell(1,0) === this.getCell(2,0) 
+        && this.isOccupied(0,0)) {
+            return this.getCell(0,0)
+        }
+
+        if (this.getCell(0,1) === this.getCell(1,1) && this.getCell(1,1) === this.getCell(2,1) 
+        && this.isOccupied(0,1)) {
+            return this.getCell(0,1)
+        }
+
+        if (this.getCell(0,2) === this.getCell(1,2) && this.getCell(1,2) === this.getCell(2,2) 
+        && this.isOccupied(0,2)) {
+            return this.getCell(0,2)
+        }
+
+        if (this.getCell(0,0) === this.getCell(1,1) && this.getCell(1,1) === this.getCell(2,2) 
+        && this.isOccupied(0,0)) {
+            return this.getCell(0,0)
+        }
+
+        if (this.getCell(0,2) === this.getCell(1,1) && this.getCell(1,0) === this.getCell(2,0) 
+        && this.isOccupied(0,2)) {
+            return this.getCell(0,2)
+        }
+
+        let allFull = true
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const cellOccupied = this.isOccupied(i, j)
+                if (!cellOccupied) {
+                    allFull = false
+                }
+            }
+        }
+
+        if (allFull) {
+            return 'draw'
+        }
+        return null
     }
 }
 
+
 const myBoard = new Board()
-myBoard.placeMarker(1, 0, new Nought())
-myBoard.placeMarker(1, 0, new Cross())
+myBoard.placeMarker(0, 0, new Nought())
+myBoard.placeMarker(0, 0, new Cross())
 myBoard.placeMarker(1, 0, new BlankMarker())
 console.log(myBoard.getCell(1,0))
